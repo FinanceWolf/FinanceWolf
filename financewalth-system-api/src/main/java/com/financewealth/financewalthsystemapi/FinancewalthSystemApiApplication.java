8d327a878d76cae7489bfa225a5a89f66296c9fa
Scoorpioon <gabrielalexandre1337@gmail.com>
2024-03-01 11:29:54 -0300

    Inserção inicial

---
+++ financewalth-system-api/src/main/java/com/financewealth/financewalthsystemapi/FinancewalthSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.financewealth.financewalthsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancewalthSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancewalthSystemApiApplication.class, args);
+	}
+
+}
---
+++ financewalth-system-api/src/test/java/com/financewealth/financewalthsystemapi/FinancewalthSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financewealth.financewalthsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancewalthSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
