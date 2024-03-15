9c0417ebb9ec221c7756878fc0f65f1406b63d22
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2024-03-15 15:05:34 -0300

    Dev mode

--- financewealth-ui/src/app/page.js
+++ financewealth-ui/src/app/page.js
@@ -1,15 +1,22 @@
+"use client"
+
 import Image from "next/image";
 import Container from "./components/Container";
 import Header from "./components/Header";
 //import Login from "./components/login/page";
 import Head from 'next/head';
+import { useState } from 'react';
+import DevContainer from './components/devmode/DevContainer';
 
 export default function Home() {
+  const [devMode, setDevMode] = useState(true);
+
   return (
     <>
-    <main className="w-full h-full flex justify-center align-middle items-center">
-      <Header />
-    </main>
+      <main className="w-full h-full flex justify-center align-middle items-center">
+        <Header />
+        {devMode ? <DevContainer path="/pages/login" name="Página de login" /> : false}
+      </main>
     </>
   );
 };
