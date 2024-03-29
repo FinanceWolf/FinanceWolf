9a8519bf4c2cca6f4afa6ed5db02ff391b465386
Gabriel Andrade <g.andrade0222@gmail.com>
2024-03-29 02:23:48 -0300

    Alteração na ladingpage e pequenas mudanças no header

---
+++ financewolf-system-api/src/main/java/com/financewolf/financewolfsystemapi/FinancewolfSystemApiApplication.java
@@ -0,0 +1,16 @@
+package com.financewolf.financewolfsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.financewolf.financewolfsystemapi.entity.Usuario;
+
+@SpringBootApplication
+public class FinancewolfSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancewolfSystemApiApplication.class, args);
+	
+	}
+
+}
---
+++ financewolf-system-api/src/test/java/com/financewolf/financewolfsystemapi/FinancewolfSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financewolf.financewolfsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancewolfSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
